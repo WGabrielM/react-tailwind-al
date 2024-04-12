4e961453e3b2257582aba418bdb64bc173d39793
Wellington Gabriel Monteiro Batista <wellingtongmb@gmail.com>
2024-04-12 11:27:08 -0300

    add dark mode

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,11 @@
-
+import ToggleTheme from "../ToggleTheme/ToggleTheme"
 
 export default function Header() {
   return (
-    <header className='flex h-20 bg-gray-500 justify-between items-center px-5 sm:rounded-xl sm:m-5'>
+    <header className='flex h-20 bg-alura-200 dark:bg-dark-200 justify-between items-center px-5 sm:rounded-xl sm:m-5'>
         <span className="text-gray-100">Olá, usuário</span>
         <h1 className="text-gray-100 text-xl">Alura Newsletter</h1>
-        <input className="hidden sm:block" type="checkbox" />
+        <ToggleTheme />
     </header>
   )
 }
